e74e5a1a97e4abdaf234ec5c9011d9320124a555
anarosaa <silvaanarosa103@gmail.com>
2022-06-28 17:24:55 -0300

    desafio5 entregue

---
+++ script.js
@@ -0,0 +1,29 @@
+// Crie um array que receba 5 itens e exiba no console.
+let dados = ['Mar','Ondas','Ar','Julia','Amanda'];
+console.log(dados);
+
+// Utilize um método para adicionar um nome ao inicio do array.
+dados.unshift('Céu');
+console.log(dados);
+
+// Utilize um método para remover o último nome do array.
+dados.pop();
+console.log(dados);
+
+// Utilize um método para adicionar dois nomes ao fim do array.
+dados.push('clara','Ana');
+console.log(dados);
+
+// Utilize um método para remover o primeiro nome do array.
+dados.shift();
+console.log(dados);
+
+
+let number = [7,5,6,3,8,9,2,1,4]
+console.log(number.sort());
+
+
+
+
+
+
